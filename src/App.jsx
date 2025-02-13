1ec0351b60b012f0a655b5160184f9d936f19945
LiamCollins <avengedsevenfold.lc@gmail.com>
2025-02-13 13:45:09 +0000

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 // import CookieDisplay from "./components/CookieDisplay.jsx"
-// import CookieButton from "./components/CookieButton.jsx"
-// import UpgradeList from "./components/UpgradeList.jsx"
+import CookieButton from "./components/CookieButton.jsx"
+import UpgradeList from "./components/UpgradeList.jsx"
 
 
 export default function App() {
 
     const [upgrades, setUpgrades] = useState({});
     const [shopData, setShopData] = useState([]);
-    const [loading, setLoading] = useState(true);
     const [cookiesPerClick, setCookiesPerClick] = useState(1);
   
     useEffect(() => {
@@ -23,17 +22,71 @@ export default function App() {
       fetchData();
     }, [])
 
+    const [count, setCount] = useState(() => {
+      let initialCookies = 0;
+      Object.keys(upgrades).forEach((type) => {
+        const upgrade = shopData.find((type) => itemm.type === type);
+        if (upgrade){
+          initialCookies =+ upgrades[type] * upgrade.cookiesPerSecond;
+        }
+      })
+      return initialCookies;
+    })
+
+    const calculateCost = ((baseCost, amountOwned) => {
+      return Math.floor(baseCost * amountOwned)
+    });
+
+    const handleClick = () => {
+        setCount(count + cookiesPerClick)
+    }
+
+    const handleBuy = (type) => {
+      const upgrade = shopData.find((type) => item.name === type)
+      if(!upgrade) return;
+    
+    console.log(upgrades)
+    const amountOwned = upgrades[type] || 0;
+    const cost = calculateCost(
+      upgrade.baseCost,
+      amountOwned
+    );
+    if (count >= cost) {
+      setCount(count - cost);
+      setUpgrades((prev) => ({ ...prev, [type]: (prev[type] || 0) + 1 }));
+
+      if (upgrade.cookiesPerClick) {
+        setCookiesPerClick(cookiesPerClick + upgrade.cookiesPerClick);
+      }
+    }
+  }
+
+
+    useEffect(() => {
+      let cookiesPerSecond = 0;
+      shopData.forEach((item) => {
+        const amountOwned = upgrades[item.type] || 0;
+        cookiesPerSecond += amountOwned * item.cookiesPerSecond;
+      })
+
+      const cpsInterval = setInterval(() => {
+        setCount((prevCount) => prevCount + cookiesPerSecond);
+      }, 1000);
+
+      return () => clearInterval(cpsInterval);
+    }, [upgrades, shopData]);
+
   return(
-    <div>
-      <h1>Shop</h1>
-      <ul>
-        {shopData.map((shop) => (
-          <div>
-            <li key={shop.id}>{shop.name}</li>
-            <li key={shop.name}>{shop.cost}</li>
-          </div>
-        ))}
-      </ul>
+    <div className="cookie-clicker-2.0">
+      <h1 className="title">Reiteration of Cookie Clicker week3</h1>
+      <CookieButton onClick={handleClick} />
+        <UpgradeList
+          count={count}
+          upgrades={upgrades}
+          shopData={shopData}
+          calculateCost={calculateCost}
+          handleBuy={handleBuy}
+        />
     </div>
-  )
+  );
 }
